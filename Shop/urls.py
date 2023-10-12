098c591e0e4e47873656e7718035a6f5057ac2e7
MathieuDionne2032450 <2032450@etu.cchic.ca>
2023-10-12 16:37:16 -0400

    get produit

--- Shop/urls.py
+++ Shop/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('Shop/', views.Accueil, name='Shop'),
-    path('detail/<ProduitName>', views.AchatDetail, name='achat'),
+    path('detail/<ProduitName>', views.AchatDetail, name='detail'),
 
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -4,16 +4,17 @@ from . import models
 
 def Accueil(request):
     produits = models.Produit.objects.all()
-    test = models.Produit(name="Poire",Description="432",prix = 5, img="fsdfr")
-    test.save()
+    
     context = {
         'prod_list': produits,
     }
     return render(request,'Accueil.html',context)
 
 def AchatDetail(request,ProduitName):
-    produit = models.Produit.objects.filter(name='name')
+   
+    produit = models.Produit.objects.all().filter(name=ProduitName).values()[0]
     context = {
+        'ProduitName':ProduitName,
         'produit': produit,
     }
     return render(request,'AchatDetail.html',context)
